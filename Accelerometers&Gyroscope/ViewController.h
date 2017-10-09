51701a359e48fe1ec4e103c79b66f925d4a4cb43
Konstantin Stolyarenko <stolyarenko.kons@mail.ru>
2017-10-09 16:39:43 +0300

    Initial Commit

---
+++ Accelerometers&Gyroscope/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Accelerometers&Gyroscope
+//
+//  Created by Kostya on 09.10.2017.
+//  Copyright © 2017 SKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
